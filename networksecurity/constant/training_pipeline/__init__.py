2952465fee4b83db85e605f541bd319b7ea5fe9e
Muhammad Asif <asifafridi3310@gmail.com>
2025-11-23 08:32:00 +0500

    Data ingestion is completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -24,4 +24,6 @@ DATA_INGESTION_COLLECTION_NAME: str = "Phising_Collection"
 DATA_INGESTION_DATABASE_NAME: str = "Asif_Network_Security_DB"
 DATA_INGESTION_DIR_NAME: str= "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_TRAIN_DIR: str = "train"
+DATA_INGESTION_TEST_DIR: str = "test"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
