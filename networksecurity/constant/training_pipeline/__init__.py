d04977bf0759af56a2ff64f2da4a4cc3a74155d5
Muhammad Asif <asifafridi3310@gmail.com>
2025-11-26 09:57:43 +0500

    first part of validation is completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -16,6 +16,8 @@ FILE_NAME: str = "phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+SCHEMA_FILE_PATH: str = os.path.join("data_schema","schema.yaml")
+
 
 """
 Data Ingestion related constant value starting with DATA_INGESTION_VAR_*
@@ -27,3 +29,17 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_TRAIN_DIR: str = "train"
 DATA_INGESTION_TEST_DIR: str = "test"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+
+
+"""
+Data Validation related constant value starting with DATA_VALIDATION_VAR_*
+
+"""
+
+DATA_VALIDATION_DIR_NAME: str= 'data_validation'
+DATA_VALIDATION_VALID_DIR: str = 'validated'
+DATA_VALIDATION_INVALID_DIR: str = 'invalid'
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = 'drift_report'
+DATA_VALIDATION_DRIFT_FILE_NAME: str = 'report.yaml'
