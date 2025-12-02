e6b60593f0451050b160b444755f61b4f94ec6cf
Muhammad Asif <asifafridi3310@gmail.com>
2025-12-02 07:45:58 +0500

    model trainer completed

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import os,sys
@@ -53,5 +55,30 @@ def load_numpy_array_data(file_path: str) ->np.array:
     try:
         with open(file_path, 'rb') as file_obj:
             return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred=model.predict(X_train)
+
+            y_test_pred=model.predict(X_test)
+
+            train_model_score=accuracy_score(y_train,y_train_pred)
+            test_model_score=accuracy_score(y_test,y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
     except Exception as e:
         raise NetworkSecurityException(e, sys)
